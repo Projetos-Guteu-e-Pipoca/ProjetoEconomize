1acb03c2091a40512ada0cc2aa66cf18e6a53e59
AlanWang2313 <146451316+AlanWang2313@users.noreply.github.com>
2024-01-07 22:41:12 -0300

    We developed the website

---
+++ script.js
@@ -0,0 +1,24 @@
+function bot(){
+    var areas = document.getElementsByName("areas")
+    var p = document.getElementById("Lporque")
+    var e = document.getElementById("Lenergia")
+    var a = document.getElementById("Lagua")
+    var d = document.getElementById("Ldia")
+    p.style.backgroundColor = "rgb(0, 186, 0)"
+    e.style.backgroundColor = "rgb(0, 186, 0)"
+    a.style.backgroundColor = "rgb(0, 186, 0)"
+    d.style.backgroundColor = "rgb(0, 186, 0)"
+    if (areas[0].checked) {
+        p.style.backgroundColor = "red"
+    }
+    if (areas[1].checked){
+        e.style.backgroundColor = "red"
+    }
+    if (areas[2].checked){
+        a.style.backgroundColor = "red"
+    }if (areas[3].checked){
+        d.style.backgroundColor = "red"
+    }
+}
+
+bot()
