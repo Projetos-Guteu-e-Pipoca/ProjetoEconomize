744a07992373f304cd757f62f3c216a0e014b121
AlanWang2313 <146451316+AlanWang2313@users.noreply.github.com>
2024-01-08 20:11:40 -0300

    Added the menu

--- script.js
+++ script.js
@@ -4,22 +4,26 @@ function bot(){
     var e = document.getElementById("Lenergia")
     var a = document.getElementById("Lagua")
     var d = document.getElementById("Ldia")
+    var p2 = document.getElementById("pq")
+    var e2 = document.getElementById("ener")
+    var a2 = document.getElementById("ag")
+    var d2 = document.getElementById("d")
     p.style.backgroundColor = "rgb(0, 186, 0)"
     e.style.backgroundColor = "rgb(0, 186, 0)"
     a.style.backgroundColor = "rgb(0, 186, 0)"
     d.style.backgroundColor = "rgb(0, 186, 0)"
     
     if (areas[0].checked) {
-        p.style.backgroundColor = "red"
+        p.style.backgroundColor = "green"
     }
     if (areas[1].checked){
-        e.style.backgroundColor = "red"
+        e.style.backgroundColor = "green"
     }
     if (areas[2].checked){
-        a.style.backgroundColor = "red"
+        a.style.backgroundColor = "green"
     }
     if (areas[3].checked){
-        d.style.backgroundColor = "red"
+        d.style.backgroundColor = "green"
     }
 }
 
